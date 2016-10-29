92604c40128fae6303c766177c50d0ae3218627e
Domantas Jurkus <domantas.jurkus@gmail.com>
2016-10-29 16:38:22 +0100

    Setting correct port + removing node modules

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ io.on('connection', function(socket) {
 	});
 });
 
-http.listen(80, function(){
-	console.log('Listening on *:80');
+http.listen(3000, function(){
+	console.log('Listening on *:3000');
 });
