6d451d79062705d3be15bd177c76c912f6f149d8
Paulius Dilkas <paulius.dilkas@gmail.com>
2016-10-30 05:24:35 +0000

    Added randomly generated rooms with persistent storage.

--- client.js
+++ client.js
@@ -1,54 +1,51 @@
 $(function () {
-	var socket = io();
-	socket.on('create', function (msg) {
-		var e = $(msg.element);
-		e.attr('id', msg.id);
-		e.draggable({
-			snap: '#board,.sb-task-note',
-			containment: "parent",
-			drag: function (event, ui) {
-				socket.emit('drag-stop', {
-					id: event.target.id,
-					left: ui.position.left,
-					top: ui.position.top
-				});
-			},
-			stop: function (event, ui) {
-				socket.emit('drag-stop', { id: event.target.id, left: ui.position.left, top: ui.position.top });
-			}
-		});
-		e.click(function () {
-			socket.emit('transform', {
-				id: this.id,
-				transform: $("input[name=transform]:checked").val(),
-				fill: $("input[name=fill]:checked").val()
-			});
-		});
-		$("#board").append(e);
-	});
+    var socket = io();
 
-	// Update dragged shapes
-	socket.on('dragging', function (msg) {
-		$("#" + msg.id).css({ left: msg.left, top: msg.top });
-	});
+    socket.on('connect', function() {
+        // room is defined by the URL
+        socket.emit('room', window.location.pathname);
+    });
 
-	socket.on('drag-stop', function (msg) {
-		$("#" + msg.id).css({ left: msg.left, top: msg.top });
-	});
-	socket.on('transform', function (msg) {
-		$("#" + msg.id)
-			.css("transform", msg.transform)
-			.css("fill", msg.fill);
-	});
-	$("#templates").children("svg").wrap("<span>");
-	$("#templates, #templates2")
-		.children()
-		.click(function () {
-			socket.emit('create', $(this).html());
-		});
-	$('#btnNewTask').on("click", function () {
-		//$('#svgBox').parent().click() //Fire the click event of box
-		$('#testNote').click();
-	})
+    socket.on('create', function (msg) {
+        var e = $(msg.element);
+        e.attr('id', msg.id);
+        e.draggable({
+            snap: '#board,.sb-task-note',
+            containment: "parent",
+            drag: function (event, ui) {
+                socket.emit('dragging', {
+                    id: event.target.id,
+                    position: ui.position
+                });
+            },
+            //stop: function (event, ui) {
+                //socket.emit('drag-stop', { id: event.target.id, left: ui.position.left, top: ui.position.top });
+            //}
+        });
+        $("#board").append(e);
+        if (msg.hasOwnProperty("position"))
+            e.css(msg.position);
+        else
+            socket.emit('dragging', {id: msg.id, position: e.position()});
+    });
+
+    // Update dragged notes
+    socket.on('dragging', function (msg) {
+        $("#" + msg.id).css(msg.position);
+    });
+
+    //socket.on('drag-stop', function (msg) {
+        //$("#" + msg.id).css({ left: msg.left, top: msg.top });
+    //});
+    $("#templates").children("svg").wrap("<span>");
+    $("#templates, #templates2")
+        .children()
+        .click(function () {
+            socket.emit('create', $(this).html());
+        });
+    $('#btnNewTask').on("click", function () {
+        //$('#svgBox').parent().click() //Fire the click event of box
+	$('#testNote').click();
+    })
 
 });
--- server.js
+++ server.js
@@ -1,38 +1,64 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var shortid = require('shortid');
+var storage = require('node-persist');
+
+// a necessary hack
+global.setImmediate = global.setImmediate || process.nextTick.bind(process);
+storage.initSync();
+storage.clearSync(); // to clear all storage, just for testing
 
 app.get('/client.js', function(req, res){
-	res.sendFile('client.js', { root: __dirname });
+    res.sendFile('client.js', { root: __dirname });
 });
 
-app.get('/', function(req, res){
-	res.sendFile('/views/testboard1.html', { root: __dirname });
+app.get('/', function(req, res) {
+    //redirect to a random room
+    res.redirect(shortid.generate());
 });
 
-var id = 0;
+app.get('*', function(req, res){
+    res.sendFile('/views/testboard1.html', { root: __dirname });
+});
+
 io.on('connection', function(socket) {
-	socket.on('create', function(msg) {
-		//console.log('create: ' + msg + "; assigning id m-" + id);
-		io.emit('create', {id: "m-" + id++, element: msg});
-	});
+    var room = "";
+    socket.on('room', function(r) {
+        room = r;
+        socket.join(r);
+        var notes = storage.getItemSync(room) || {};
+        for (var noteId in notes) {
+            // recreate notes from storage
+            io.sockets.to(socket.id).emit('create', notes[noteId]);
+        }
+    });
+    socket.on('create', function(msg) {
+        var notes = storage.getItemSync(room) || {};
+        var idTag = "m-" + Object.keys(notes).length;
+        //console.log('create: ' + msg + "; assigning id " + idTag);
+        notes[idTag] = {id: idTag, element: msg};
+        io.sockets.in(room).emit('create', notes[idTag]);
+        storage.setItemSync(room, notes);
+    });
 
-	// Broadcast shape movement
-	socket.on('draging', function(msg) {
-		//console.log('draging: ' + msg.id + " to " + msg.top + " " + msg.left);
-		socket.broadcast.emit('draging', msg);
-	});
+    // Broadcast note movement
+    socket.on('dragging', function(msg) {
+        //console.log('dragging: ' + msg.id + " to " + msg.position.top + " " + msg.position.left);
+        var notes = storage.getItemSync(room) || {};
+        if (msg.id in notes) {
+            notes[msg.id].position = msg.position;
+            socket.broadcast.in(room).emit('dragging', msg);
+            storage.setItemSync(room, notes);
+        }
+    });
 
-	socket.on('drag-stop', function(msg) {
-		//console.log('drag-stop: ' + msg.id + " to " + msg.top + " " + msg.left);
-		socket.broadcast.emit('drag-stop', msg);
-	});
-	socket.on('transform', function(msg) {
-		//console.log('transform: ' + msg);
-		io.emit('transform', msg);
-	});
+    //socket.on('drag-stop', function(msg) {
+        //console.log('drag-stop: ' + msg.id + " to " + msg.top + " " + msg.left);
+        //socket.broadcast.emit('drag-stop', msg);
+    //});
 });
 
 http.listen(3000, function() {
-	console.log('Listening on *:3000');
+    console.log('Listening on *:3000');
 });
