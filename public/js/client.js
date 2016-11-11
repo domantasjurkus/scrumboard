94c62b5a0400cc37415233ef3a62e69133d7b3aa
Domantas Jurkus <domantas.jurkus@gmail.com>
2016-11-11 14:56:56 +0000

    Split note into separate html file

--- public/js/client.js
+++ public/js/client.js
@@ -8,63 +8,73 @@ $(function () {
         for (var i = 0; i < 4; i++) {
             $('#header' + i).editable("click", function(e) {
                 socket.emit('edit-header', {id: e.target.attr('id'),
-                                            text: e.value});
+                    text: e.value});
             });
         }
     });
 
     socket.on('create', function (msg) {
-        var e = $('<div class="sb-task-note"><div class="sb-delete"><i class="fa fa-2x fa-times" aria-hidden="true"></i></div><h3 style="margin: 8px;"></h3></div>');
-        e.children('.sb-delete').on("click",function(){
-            socket.emit('delete', msg.id);
-        });
-        e.attr('id', msg.id);
-        e.resizable({
-            containment: 'parent',
-            resize: function (event, ui) {
-                socket.emit('resizing', {id: event.target.id, size: ui.size});
-            },
-            start: function(event, ui) {
-                socket.emit('resize-start', {id: event.target.id,
-                                             size: ui.originalSize});
-            }
-        });
-        e.draggable({
-            //snap: '.sb-task-note,.sb-board-region-title,.sb-board-region-title-alt',
-            snap: '.snap-region',
-            snapMode: 'inner',
-            containment: "#board",
-            snapTolerance: 25,
-            start: function(event, ui) {
-                $(this).addClass('noclick');
-                socket.emit('drag-start', {id: event.target.id,
-                                           position: ui.originalPosition});
-            },
-            drag: function (event, ui) {
-                socket.emit('dragging', {
-                    id: event.target.id,
-                    parent: $(this).parent().attr('id'),
-                    position: ui.position
+        var noteMarkup;
+        $.ajax({
+            url: '/note',
+            success: function(markup) {
+                //console.log(data);
+                //noteMarkup = data;
+                //var e = $('<div class="sb-task-note"><div class="sb-delete"><i class="fa fa-2x fa-times" aria-hidden="true"></i></div><h3 style="margin: 8px;"></h3></div>');
+                e = $(markup);
+                e.children('.sb-delete').on("click",function(){
+                    socket.emit('delete', msg.id);
                 });
-            },
-            stop: function(event, ui) {
-                socket.emit('drag-stop', event.target.id);
+                e.attr('id', msg.id);
+                e.resizable({
+                    containment: 'parent',
+                    resize: function (event, ui) {
+                        socket.emit('resizing', {id: event.target.id, size: ui.size});
+                    },
+                    start: function(event, ui) {
+                        socket.emit('resize-start', {id: event.target.id,
+                           size: ui.originalSize});
+                    }
+                });
+                e.draggable({
+                //snap: '.sb-task-note,.sb-board-region-title,.sb-board-region-title-alt',
+                snap: '.snap-region',
+                snapMode: 'inner',
+                containment: "#board",
+                snapTolerance: 25,
+                start: function(event, ui) {
+                        $(this).addClass('noclick');
+                        socket.emit('drag-start', {id: event.target.id,
+                        position: ui.originalPosition});
+                    },
+                    drag: function (event, ui) {
+                        socket.emit('dragging', {
+                            id: event.target.id,
+                            parent: $(this).parent().attr('id'),
+                            position: ui.position
+                        });
+                    },
+                    stop: function(event, ui) {
+                        socket.emit('drag-stop', event.target.id);
+                    }
+                });
+                e.find("h3").html(msg.text);
+                e.find("h3").editable({type: "textarea", action: "click"},
+                  function(e) {
+                      socket.emit('edit', {
+                          id: msg.id,
+                          text: e.value.replace(/\n/g, '<br />')
+                      });
+                  });
+                e.css(msg.position);
+
+                if (msg.hasOwnProperty('size'))
+                    e.css(msg.size);
+
+                $("#" + msg.parent).append(e);
             }
         });
-        e.find("h3").html(msg.text);
-        e.find("h3").editable({type: "textarea", action: "click"},
-                              function(e) {
-                                  socket.emit('edit', {
-                                      id: msg.id,
-                                      text: e.value.replace(/\n/g, '<br />')
-                                  });
-                              });
-        e.css(msg.position);
-
-        if (msg.hasOwnProperty('size'))
-            e.css(msg.size);
         
-        $("#" + msg.parent).append(e);
     });
 
     // Update dragged notes
@@ -104,8 +114,8 @@ $(function () {
 
     $('#btnNewTask').on("click", function () {
         socket.emit('create', {text: 'Your New Task!',
-                               position: {top: 0, left: 0},
-                               parent: 'backlog'});
+         position: {top: 0, left: 0},
+         parent: 'backlog'});
     });
     $('#btnUndo').on("click", function() {
         socket.emit('undo');
@@ -119,11 +129,11 @@ $(function () {
 
     $('#btnShare').on("click", function() {
     	var pic = "static/images/img" +  (Math.floor(Math.random() * 5) + 1) + ".jpg"; 
-       	swal({
-  	    title: "Your URL:",
-  	    text: window.location.href,
-  	    imageUrl: pic
-	});
+        swal({
+         title: "Your URL:",
+         text: window.location.href,
+         imageUrl: pic
+     });
     });
 
 });
